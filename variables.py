b68aa99e357c8fc5ec599891321425cc1bfa3b24
Kamal9890 <119168619+Kamal9890@users.noreply.github.com>
2023-09-18 16:43:54 +0530

    Create variables.py
    It is Python code for data science

---
+++ variables.py
@@ -0,0 +1,31 @@
+rent = 8000
+rent
+8000
+petrol = 500.5
+groceries = 2050.5
+petrol
+500.5
+total = rent+petrol+groceries
+total
+10551.0
+gabbar = "amjad khan"
+jay = "amitabh"
+gabbar
+'amjad khan'
+type(gabbar)
+<class 'str'>
+type(rent)
+<class 'int'>
+learn_python = True
+learn_fortan = false
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    learn_fortan = false
+NameError: name 'false' is not defined. Did you mean: 'False'?
+learn_python
+learn_python
+True
+
+learn_fortan = False
+learn_python
+True
