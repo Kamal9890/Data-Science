a836d66444e429ca9a28d31f69fefccb6c1147dc
Kamal9890 <119168619+Kamal9890@users.noreply.github.com>
2023-09-18 16:56:18 +0530

    Create variableexcercize.py

---
+++ variableexcercize.py
@@ -0,0 +1,16 @@
+break = 5
+SyntaxError: invalid syntax
+birth_year = 2002
+current_year = 2023
+age = current_year-birth_year
+age
+21
+
+
+first = "Kamal"
+middle ="kumar"
+last = "pandey"
+print("my full name is : " "+first+" "+middle+" "+last")
+my full name is : +first++middle++last
+print("my full name is : " " "+first+ " "+middle+ " "+last)
+my full name is :  Kamal kumar pandey
