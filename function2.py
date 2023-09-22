1f21c5ca6669372fa8dc7d60f0759358d4ad5161
Kamal9890 <119168619+Kamal9890@users.noreply.github.com>
2023-09-22 15:57:04 +0530

    Create function2.py

---
+++ function2.py
@@ -0,0 +1,11 @@
+def cyclinder_volume(radius,height):
+    print("radius", radius)
+    print("height", height)
+    
+    volume = 3.14*(radius**2)*height
+    return volume
+    
+r= 5
+h= 10
+
+print(cyclinder_volume(r,h))
